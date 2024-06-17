035c55c86d74a3071378883028a4225251be08df
vibradeporte <info@univlearning.com>
2024-06-16 23:58:31 -0500

    Update upload_matricula.py

--- routers/upload_matricula.py
+++ routers/upload_matricula.py
@@ -155,4 +155,4 @@ def upload_file(file: UploadFile = File(...)):
     correos_validar.to_csv('temp_files/correos_validar.csv',index = False,header = False)
     validated_df.to_excel('temp_files/validacion_inicial.xlsx',index = False)
 
-    return {"filename": file.filename,"validation": "success","Número de Estudiantes que no seran aprobados": si_rows_count, "Estudiantes a matricular": matriculated_students}
+    return {"filename": file.filename,"validation": "success","Número de Estudiantes que no seran matriculados": si_rows_count, "Estudiantes a matricular": matriculated_students}
