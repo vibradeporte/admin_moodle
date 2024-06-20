0fc08cc69d25230808217fbc6eeda323de269b81
vibradeporte <info@univlearning.com>
2024-06-20 15:06:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app.title = "Universal Learning ADMIN MOODLE API "
 app.version = "0.0.1"
 
 app.include_router(upload_file_matricula)
+app.include_router(verificar_correos)
 
 @app.get('/', tags=['home'])
 def message():
