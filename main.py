797c5dcc8f1fb53c3bf7f8b4564e29f9108422bb
vibradeporte <info@univlearning.com>
2024-06-28 10:54:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import requests
 import logging
-from routers.upload_matricula import upload_file_matricula
-from routers.verificar_correos import verificar_correos
-from routers.validar_cursos import validacion_cursos
-from routers.validacion_final import validacion_final
+#from routers.upload_matricula import upload_file_matricula
+#from routers.verificar_correos import verificar_correos
+#from routers.validar_cursos import validacion_cursos
+#from routers.validacion_final import validacion_final
 from routers.subida_Archivos import verificacion_inicial_archivo
 from routers.validacion_identidad import identificacion_usuario
 
@@ -22,10 +22,10 @@ app.include_router(identificacion_usuario)
 app.include_router(verificacion_inicial_archivo)
 # app.include_router(core_user_create_users_router)
 # app.include_router(enrol_manual_enrol_users_router)
-app.include_router(upload_file_matricula)
-app.include_router(verificar_correos)
-app.include_router(validacion_cursos)
-app.include_router(validacion_final)
+#app.include_router(upload_file_matricula)
+#app.include_router(verificar_correos)
+#app.include_router(validacion_cursos)
+#app.include_router(validacion_final)
 
 @app.get('/', tags=['home'])
 def message():
