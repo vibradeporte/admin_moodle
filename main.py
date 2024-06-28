47bf97d1ac8aff4a768b1b466ff741dcb81b9578
vibradeporte <info@univlearning.com>
2024-06-28 10:32:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from routers.verificar_correos import verificar_correos
 from routers.validar_cursos import validacion_cursos
 from routers.validacion_final import validacion_final
 from routers.subida_Archivos import verificacion_inicial_archivo
-from routers.validacion_identidad import identificacion_usuario
+#from routers.validacion_identidad import identificacion_usuario
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ app = FastAPI(
 )
 
 
-app.include_router(identificacion_usuario)
+#app.include_router(identificacion_usuario)
 app.include_router(verificacion_inicial_archivo)
 # app.include_router(core_user_create_users_router)
 # app.include_router(enrol_manual_enrol_users_router)
