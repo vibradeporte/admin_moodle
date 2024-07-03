892b35d0e1a94c4c6e08207863bedeed149e78c4
vibradeporte <info@univlearning.com>
2024-07-03 14:10:00 -0500

    Update validacion_identidad.py

--- routers/validacion_identidad.py
+++ routers/validacion_identidad.py
@@ -5,6 +5,9 @@ from urllib.parse import quote_plus
 from dotenv import load_dotenv
 import os
 
+
+identificacion_usuario = APIRouter()
+
 load_dotenv()
 usuario = os.getenv("USER_DB_UL_ADMIN")
 contrasena = os.getenv("PASS_DB_UL_ADMIN")
@@ -13,7 +16,6 @@ nombre_base_datos = os.getenv("NAME_DB_UL_ADMIN")
 contrasena_codificada = quote_plus(contrasena)
 
 DATABASE_URL = f"mysql+mysqlconnector://{usuario}:{contrasena_codificada}@{host}/{nombre_base_datos}"
-identificacion_usuario = APIRouter()
 engine = create_engine(DATABASE_URL)
 
 @identificacion_usuario.get("/user/{user_id}", tags=['Validacion_Identidad'])
@@ -37,30 +39,23 @@ def encontrar_usuario(user_id: int):
         WHERE 
             usuario.ID_USUARIO = :user_id
     """)
+        result = connection.execute(consulta_sql)
+        rows = result.fetchall()
+        column_names = result.keys()
 
-    try:
-        with engine.connect() as connection:
-            result = connection.execute(query, {'user_id': user_id}).fetchone()
-            if result:
-                user_data = {
-                    "ID_USUARIO": result.ID_USUARIO,
-                    "IDENTIFICACION": result.IDENTIFICACION,
-                    "MOVIL": result.MOVIL,
-                    "CORREO": result.CORREO,
-                    "URL_MOODLE": result.URL_MOODLE,
-                    "TOKEN_MOODLE": result.TOKEN_MOODLE,
-                    "CADENA_CONEXION_BD": result.CADENA_CONEXION_BD,
-                    "ID_PERMISO": result.ID_PERMISO
-                }
-                return user_data
-            else:
-                raise HTTPException(status_code=404, detail="Usuario No Encontrado")
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail="Error interno del servidor: Error en la base de datos")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Error interno del servidor")
+        
+        result_dicts = []
+        for row in rows:
+            row_dict = dict(zip(column_names, row))
+            
+            result_dicts.append(row_dict)
 
-app.include_router(identificacion_usuario)
+        if result_dicts:
+            return JSONResponse(content=result_dicts)
+        else:
+            codigo = SIN_INFORMACION
+            mensaje = HTTP_MESSAGES.get(codigo)
+            raise HTTPException(codigo, mensaje)
 
 
 
