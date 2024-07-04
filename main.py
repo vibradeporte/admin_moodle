896253f4fdc1021ffaa9ef009ce5c3ea93b88aea
vibradeporte <info@univlearning.com>
2024-07-04 15:07:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from routers.validacion_cedula import *
 from routers.validacion_num_wapp import *
 from routers.normalizacion import *
 from routers.validacion_final import *
+from routers.validar_cursos_certificado import *
 
 #moodle
 from routers.est_matriculados_curso_con_certificados import est_matriculados_curso_con_certificados_router
@@ -36,7 +37,7 @@ app.include_router(verificar_correos)
 app.include_router(validacion_numeros_whatsapp_router)
 app.include_router(normalizacion_router)
 app.include_router(validacion_final)
-
+app.include_router(validacion_cursos_certificado_router)
 
 #moodle
 app.include_router(est_matriculados_curso_con_certificados_router)
