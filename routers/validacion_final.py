40e4a399b2c462aa41d5d83b5ae2c0483c61ac20
vibradeporte <info@univlearning.com>
2024-07-05 12:35:56 -0500

    Update validacion_final.py

--- routers/validacion_final.py
+++ routers/validacion_final.py
@@ -165,10 +165,14 @@ async def validate_students():
     BD_USUARIOS.sort_values('username', inplace=True)
     
     nombreColumnaQueRegistraSiElEstudEstaEnLaBD = 'Estado'
-    estudiantes_matricular[nombreColumnaQueRegistraSiElEstudEstaEnLaBD] = 'NO está en la BD esa cédula'
+    estudiantes_a_matricular = estudiantes_matricular[
+    (estudiantes_matricular['nombreColumnaQueRegistraSiElEstudEstaEnLaBD'] == 'NO está en la BD esa cédula') |
+    (estudiantes_matricular['nombreColumnaQueRegistraSiElEstudEstaEnLaBD'] == 'SI')]
     resultado = procesar_matriculas(estudiantes_matricular, BD_USUARIOS, nombreColumnaQueRegistraSiElEstudEstaEnLaBD)
     estudiantes_matricular.to_csv('temp_files/estudiantes_validados.csv', index=False)
-    estudiantes_que_no_seran_matriculados = estudiantes_matricular[estudiantes_matricular[nombreColumnaQueRegistraSiElEstudEstaEnLaBD] != 'NO está en la BD esa cédula']
+    estudiantes_que_no_seran_matriculados = estudiantes_matricular[
+    (estudiantes_matricular[nombreColumnaQueRegistraSiElEstudEstaEnLaBD] != 'NO está en la BD esa cédula') &
+    (estudiantes_matricular[nombreColumnaQueRegistraSiElEstudEstaEnLaBD] != 'SI')]
     estudiantes_que_no_seran_matriculados.to_excel('temp_files/estudiantes_invalidos.xlsx', index=False)
 
     inconsistencias = estudiantes_matricular[estudiantes_matricular[nombreColumnaQueRegistraSiElEstudEstaEnLaBD] != 'NO está en la BD esa cédula'].shape[0]
