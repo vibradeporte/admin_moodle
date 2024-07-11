8a83e2815bf61ccf47d4c35d4ad271e36bdd87b6
vibradeporte <info@univlearning.com>
2024-07-11 01:31:41 -0500

    Delete routers directory

--- routers/prueba csv.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8000/enrol_users_from_csv/'
-files = {'file': open('path/to/estudiantes_validados.csv', 'rb')}
-
-response = requests.post(url, files=files)
-
-print(response.json())
