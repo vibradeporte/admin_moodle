bb9a4e2ef3d971997a431f0a08a2d756a50effba
vibradeporte <info@univlearning.com>
2024-07-11 01:57:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,37 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import requests
 
+#Validacion Inicial
+from routers.validacion_identidad import *
 from routers.validacion_nombres_apellidos import *
 from routers.verificar_correos import *
 from routers.subida_Archivos import *
-from routers.matricular_Usuarios import *
-from routers.prueba_user_id import prueba_conseguir_id
 from routers.validacion_cedula import *
+
+#Cursos
+from routers.validar_cursos_certificado import *
+from routers.duracion_curso_y_descripcion import duracion_curso_y_descripcion_router
+from routers.nombres_cursos import nombres_cursos_router
+
+#Validacion Secundaria
 from routers.validacion_num_wapp import *
 from routers.normalizacion import *
 from routers.validacion_final import *
-from routers.validar_cursos_certificado import *
-from routers.validacion_identidad import *
 
-#from  routers.core_group_create_groups import *
 
+
+#Grupos
+from  routers.core_group_create_groups import *
+from  routers.core_group_add_group_members import *
+#Estudiantes
 from routers.core_user_create_users import *
-
 from routers.enrol_manual_enrol_users import *
+from routers.prueba_user_id import prueba_conseguir_id
+from routers.usuarios_bd import usuarios_bd_router
 
 
 #moodle
 from routers.est_matriculados_curso_con_certificados import est_matriculados_curso_con_certificados_router
-from routers.usuarios_bd import usuarios_bd_router
-from routers.duracion_curso_y_descripcion import duracion_curso_y_descripcion_router
-from routers.nombres_cursos import nombres_cursos_router
 
 
 #Whatsapp
@@ -41,7 +48,11 @@ app = FastAPI(
     version="0.0.1"
 )
 
-#app.include_router(core_group_create_groups_router)
+
+#Grupos
+app.include_router(core_group_create_groups_router)
+app.include_router(core_group_add_group_members_router)
+
 
 app.include_router(enrol_manual_enrol_users_router)
 
@@ -68,9 +79,12 @@ app.include_router(nombres_cursos_router)
 
 #whatsapp
 app.include_router(envio_mensajes_whatsapp_router)
+app.include_router(envio_mensajes_whatsapp_bienvenida_router)
+
 @app.get('/', tags=['home'])
 def message():
     response = requests.get('https://ipinfo.io/ip')
     ip_address = response.text.strip()
     print(ip_address)
     return HTMLResponse(f'<h1>Universal Learning ADMIN MOODLE API</h1><p>Client IP: {ip_address}</p>')
+
