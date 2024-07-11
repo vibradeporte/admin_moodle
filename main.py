ac4403ada3ea1cb8b33b29173fb37931529a6bb5
vibradeporte <info@univlearning.com>
2024-07-11 09:37:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ from routers.validacion_final import *
 #JSON
 from routers.Parametros_bienvenida import *
 
-
 #Grupos
 from  routers.core_group_create_groups import *
 from  routers.core_group_add_group_members import *
@@ -35,6 +34,8 @@ from routers.usuarios_bd import usuarios_bd_router
 
 #moodle
 from routers.est_matriculados_curso_con_certificados import est_matriculados_curso_con_certificados_router
+
+
 
 
 #Whatsapp
@@ -55,6 +56,9 @@ app = FastAPI(
 app.include_router(core_group_create_groups_router)
 app.include_router(core_group_add_group_members_router)
 
+#JSON
+app.include_router(Bienvenida_wapp_estudiantes_router)
+
 
 app.include_router(enrol_manual_enrol_users_router)
 
@@ -64,10 +68,7 @@ app.include_router(identificacion_usuario)
 
 app.include_router(prueba_conseguir_id)
 app.include_router(validacion_cedula_router)
-
-#JSON
-app.include_router(Bienvenida_wapp_estudiantes_router)
-
+app.include_router(enrol_manual_enrol_users_router)
 app.include_router(verificacion_inicial_archivo)
 app.include_router(validacion_nombres_apellidos_router)
 app.include_router(verificar_correos)
@@ -91,4 +92,3 @@ def message():
     ip_address = response.text.strip()
     print(ip_address)
     return HTMLResponse(f'<h1>Universal Learning ADMIN MOODLE API</h1><p>Client IP: {ip_address}</p>')
-
