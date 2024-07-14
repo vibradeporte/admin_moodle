033d929d3e6443654a11e9a4d33189b918a27ab5
vibradeporte <info@univlearning.com>
2024-07-13 23:00:00 -0500

    Update Envio_Correo.py

--- routers/Envio_Correo.py
+++ routers/Envio_Correo.py
@@ -2,11 +2,9 @@ import os
 import requests
 from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import List
-
-
+import pandas as pd
 
 load_dotenv()
 AUTH_KEY = os.getenv("AUTH_KEY")
@@ -35,6 +33,7 @@ correo_router = APIRouter()
 
 @correo_router.post("/send_email", tags=['correo'], status_code=200)
 def enviar_correos(batch: EmailBatchSchema):
+    message_ids = []
 
     for email in batch.emails:
         data = {
@@ -58,7 +57,16 @@ def enviar_correos(batch: EmailBatchSchema):
             raise HTTPException(status_code=500, detail=f"Error sending email to {email.to}: {str(e)}")
 
         if response.status_code != 200:
-
             raise HTTPException(status_code=response.status_code, detail=f"Error sending email to {email.to}: {response.text}")
 
-    return {"message": "Todos los correos fueron enviados exitosamente."}
+        response_data = response.json()
+        message_id = response_data.get('mid')
+        if message_id:
+            message_ids.append(message_id)
+
+    # Save message_ids to a CSV file using pandas
+    df = pd.DataFrame(message_ids, columns=["message_id"])
+    csv_file_path = "temp_files/message_ids.csv"
+    df.to_csv(csv_file_path, index=False)
+
+    return {"message": "Todos los correos fueron enviados exitosamente.", "message_ids": message_ids, "csv_file_path": csv_file_path}
