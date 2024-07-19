1c445eee6633df162d9ade7c69d1059577bc87e0
vibradeporte <info@univlearning.com>
2024-07-19 12:23:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,86 +2,81 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import requests
 
-#Archivos
-from routers.Archivo_base64 import *
+# Archivos
+from routers.Archivo_base64 import Archivo_base64_router
 
-#Validacion Inicial
-from routers.validacion_identidad import *
-from routers.validacion_nombres_apellidos import *
-from routers.subida_Archivos import *
-from routers.validacion_cedula import *
+# Validacion Inicial
+from routers.validacion_identidad import identificacion_usuario
+from routers.validacion_nombres_apellidos import validacion_nombres_apellidos_router
+from routers.subida_Archivos import verificacion_inicial_archivo
+from routers.validacion_cedula import validacion_cedula_router
 
-#CORREOS
-from routers.Envio_Correo import *
-from routers.verificar_correos import *
-from routers.Envio_Correo_Archivo_Adjunto import *
-#Cursos
-from routers.validar_cursos_certificado import *
+# Correos
+from routers.Envio_Correo import correo_router
+from routers.verificar_correos import verificar_correos
+from routers.Envio_Correo_Archivo_Adjunto import correo_archivo_adjunto_router
+
+# Cursos
+from routers.validar_cursos_certificado import validacion_cursos_certificado_router_prueba
 from routers.duracion_curso_y_descripcion import duracion_curso_y_descripcion_router
 from routers.nombres_cursos import nombres_cursos_router
 
-#Validacion Secundaria
-from routers.validacion_num_wapp import *
-from routers.normalizacion import *
-from routers.validacion_final import *
+# Validacion Secundaria
+from routers.validacion_num_wapp import validacion_numeros_whatsapp_router
+from routers.normalizacion import normalizacion_router
+from routers.validacion_final import validacion_final
 
-#JSON
-from routers.Parametros_bienvenida import *
-from routers.estructura_correo_bienvenida import *
+# JSON
+from routers.Parametros_bienvenida import Bienvenida_wapp_estudiantes_router
+from routers.estructura_correo_bienvenida import Bienvenida_correo_estudiantes_router
 
-#Grupos
-from  routers.core_group_create_groups import *
-from  routers.core_group_add_group_members import *
-#Estudiantes
-from routers.core_user_create_users import *
-from routers.enrol_manual_enrol_users import *
+# Grupos
+from routers.core_group_create_groups import core_group_create_groups_router
+from routers.core_group_add_group_members import core_group_add_group_members_router
+
+# Estudiantes
+from routers.core_user_create_users import core_user_create_users_router
+from routers.enrol_manual_enrol_users import enrol_manual_enrol_users_router
 from routers.prueba_user_id import prueba_conseguir_id
 from routers.usuarios_bd import usuarios_bd_router
 
 
+# SQL
+from routers.poblar_tabla_matricula import poblar_tabla_matricula_router
+from routers.poblar_tabla_detalle_matricula import poblar_tabla_detalle_matricula_router
 
-
-#SQL
-from routers.poblar_tabla_matricula import *
-from routers.poblar_tabla_detalle_matricula import *
-#Whatsapp
-from routers.envio_mensajes_whatsapp import *
-
-app = FastAPI()
-app.title = "Universal Learning ADMIN MOODLE API "
-app.version = "0.0.1"
-
+# WhatsApp
+from routers.envio_mensajes_whatsapp import envio_mensajes_whatsapp_router
 
 app = FastAPI(
     title="Universal Learning ADMIN MOODLE API",
     version="0.0.1"
 )
-#Archivos
+
+# Including routers
+# Archivos
 app.include_router(Archivo_base64_router)
 
-
-#SQL
+# SQL
 app.include_router(poblar_tabla_matricula_router)
 app.include_router(poblar_tabla_detalle_matricula_router)
 
-#Grupos
+# Grupos
 app.include_router(core_group_create_groups_router)
 app.include_router(core_group_add_group_members_router)
 
-#JSON
+# JSON
 app.include_router(Bienvenida_wapp_estudiantes_router)
 
-#CORREO
+# Correos
 app.include_router(correo_router)
 app.include_router(Bienvenida_correo_estudiantes_router)
 app.include_router(correo_archivo_adjunto_router)
 
+# Estudiantes
 app.include_router(enrol_manual_enrol_users_router)
-
 app.include_router(core_user_create_users_router)
-
 app.include_router(identificacion_usuario)
-
 app.include_router(prueba_conseguir_id)
 app.include_router(validacion_cedula_router)
 app.include_router(verificacion_inicial_archivo)
@@ -92,17 +87,17 @@ app.include_router(normalizacion_router)
 app.include_router(validacion_final)
 app.include_router(validacion_cursos_certificado_router_prueba)
 
-#moodle
+# Moodle
 app.include_router(usuarios_bd_router)
 app.include_router(duracion_curso_y_descripcion_router)
 app.include_router(nombres_cursos_router)
 
-#whatsapp
+# WhatsApp
 app.include_router(envio_mensajes_whatsapp_router)
 
 @app.get('/', tags=['home'])
 def message():
     response = requests.get('https://ipinfo.io/ip')
     ip_address = response.text.strip()
-    print(ip_address)
     return HTMLResponse(f'<h1>Universal Learning ADMIN MOODLE API</h1><p>Client IP: {ip_address}</p>')
+
