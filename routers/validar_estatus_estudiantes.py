bd91b229ee55f774116e7cd041a9a6adbc3fa212
vibradeporte <info@univlearning.com>
2024-08-08 00:35:05 -0500

    Update validar_estatus_estudiantes.py

--- routers/validar_estatus_estudiantes.py
+++ routers/validar_estatus_estudiantes.py
@@ -6,17 +6,13 @@ from urllib.parse import quote_plus
 import pandas as pd
 from datetime import datetime
 
-
 validacion_inicial_file_path = 'temp_files/validacion_inicial.xlsx'
 validacion_estudiantes_estatus_router = APIRouter()
 
 
 def get_database_url(user: str, password: str, host: str, db_name: str, port: int = None) -> str:
     password_encoded = quote_plus(password)
-    if port:
-        return f"mysql+mysqlconnector://{user}:{password_encoded}@{host}:{port}/{db_name}"
-    else:
-        return f"mysql+mysqlconnector://{user}:{password_encoded}@{host}/{db_name}"
+    return f"mysql+mysqlconnector://{user}:{password_encoded}@{host}:{port}/{db_name}"
 
 
 def estudiantes_estatus(cursos: list, usuario: str, contrasena: str, host: str, port: str, nombre_base_datos: str) -> pd.DataFrame:
@@ -57,7 +53,7 @@ def estudiantes_estatus(cursos: list, usuario: str, contrasena: str, host: str, 
         AND r.shortname = 'student'
         AND u.username REGEXP '^[0-9]+$';
     """)
-    
+
     with engine.connect() as connection:
         result = connection.execute(consulta_sql)
         rows = result.fetchall()
@@ -69,6 +65,7 @@ def estudiantes_estatus(cursos: list, usuario: str, contrasena: str, host: str, 
     result_dicts = [dict(zip(column_names, row)) for row in rows]
     return pd.DataFrame(result_dicts)
 
+
 @validacion_estudiantes_estatus_router.post("/validar_estatus/", tags=['Validacion_Final'])
 async def validacion_estudiantes_estatus_final(usuario: str, contrasena: str, host: str, port: str, nombre_base_datos: str):
     try:
@@ -79,42 +76,32 @@ async def validacion_estudiantes_estatus_final(usuario: str, contrasena: str, ho
         cursos_unicos = datos_df['NOMBRE_CORTO_CURSO'].unique().tolist()
         estudiantes_estatus_df = estudiantes_estatus(cursos_unicos, usuario, contrasena, host, port, nombre_base_datos)
 
-
         # Merge the dataframes
-        resultado = datos_df.merge(estudiantes_estatus_df, left_on='IDENTIFICACION', right_on='username', how='left', suffixes=('', '_estatus'))
+        resultado = datos_df.merge(estudiantes_estatus_df, left_on=['IDENTIFICACION', 'NOMBRE_CORTO_CURSO'], right_on=['username', 'courseshortname'], how='left', suffixes=('', '_estatus'))
+
         resultado.drop_duplicates(subset=['IDENTIFICACION', 'NOMBRE_CORTO_CURSO'], inplace=True)
         resultado['Esta_activo_estudiante'] = resultado['ESTA_ACTIVO'].apply(lambda x: 'SI' if x == 1 else 'NO')
 
         resultado['lastnamephonetic'] = resultado.apply(
             lambda row: f"{row['courseshortname']}|{row['enrolment_start_date'].strftime('%d/%m/%Y')}|{row['enrolment_end_date'].strftime('%d/%m/%Y')}" if row['ESTA_ACTIVO'] == 0 else '', axis=1)
 
-
         resultado = resultado.replace({pd.NA: None, float('inf'): None, float('-inf'): None})
 
         columns_to_drop = [
-            'CourseShortName_estatus.1', 'CourseFullName_estatus.1', 'UserCedula_estatus.1', 
-            'UserNombre_estatus.1', 'UserApellido_estatus.1', 'CertificadoFechaEmision_estatus.1', 
-            'CertificadoCodigo_estatus.1','CourseShortName_estatus', 'CourseFullName_estatus', 'UserCedula_estatus', 
-            'UserNombre_estatus', 'UserApellido_estatus', 'CertificadoFechaEmision_estatus', 
-            'CertificadoCodigo_estatus','courseshortname','username','first_name','last_name','enrolment_status',
-            'enrolment_start_date','enrolment_end_date','ESTA_ACTIVO'
-
+            'courseshortname', 'username', 'first_name', 'last_name', 'enrolment_status',
+            'enrolment_start_date', 'enrolment_end_date', 'ESTA_ACTIVO'
         ]
         resultado.drop(columns=columns_to_drop, inplace=True, errors='ignore')
         resultado.to_excel(validacion_inicial_file_path, index=False)
 
-
         message = (
-            f"Verificación de estatus Terminada\n"
-            
+            "Verificación de estatus Terminada\n"
+            f"{len(resultado)} estudiantes verificados\n"
         )
 
         return PlainTextResponse(content=message)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
-
-
-
 
 
 
