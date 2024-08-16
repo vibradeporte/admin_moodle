92d923bf30c99f7c9d8c9bb41cf268d5d30d82ed
vibradeporte <info@univlearning.com>
2024-08-16 16:23:36 -0500

    Update validacion_nombres_apellidos.py

--- routers/validacion_nombres_apellidos.py
+++ routers/validacion_nombres_apellidos.py
@@ -1,18 +1,12 @@
-from fastapi import FastAPI, File, UploadFile,APIRouter,HTTPException
-from fastapi.responses import HTMLResponse,JSONResponse,PlainTextResponse
+from fastapi import FastAPI, File, UploadFile, APIRouter, HTTPException
+from fastapi.responses import PlainTextResponse
 import pandas as pd
-from io import StringIO ,BytesIO
+from io import BytesIO
 import re
-import time
-import os
-import json
 import unidecode
 import nltk
-import requests
 from nltk.tokenize import word_tokenize
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-from collections import Counter
+import os
 nltk.download('punkt')
 
 validacion_nombres_apellidos_router = APIRouter()
@@ -20,7 +14,7 @@ validacion_nombres_apellidos_router = APIRouter()
 def verificar_cruzados(row):
     columnas = [
         'pimer_nombre_es_apellido', 'segundo_nombre_es_apellido',
-        'pimer_apellido_es_nombre', 'segundo_apellido_es_nombre'
+        'primer_apellido_es_nombre', 'segundo_apellido_es_nombre'
     ]
 
     num_trues = sum(row[col] for col in columnas)
@@ -29,7 +23,7 @@ def verificar_cruzados(row):
     elif num_trues >= 3:
         return 'SI'
     else:
-        return 'ES PROBABLE'
+        return 'SI'
 
 def validar_nombre_apellido(s):
     if not s or not re.match("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$", s):
@@ -38,15 +32,15 @@ def validar_nombre_apellido(s):
         return "SI"
     else:
         return "NO"
-    
 
-def encontrar_similitudes(primer_token, otros_nombres):
-    return primer_token in otros_nombres
+def encontrar_similitudes(token, lista):
+    return token in lista
 
 def nuevo_estan_cruzados(datos):
     fuente_de_busqueda = "routers/Nombres y apellidos.xlsx"
     df = pd.read_excel(fuente_de_busqueda)
 
+    # Normalize names and surnames
     datos['NOMBRES'] = datos["NOMBRES"].str.strip().apply(unidecode.unidecode).str.upper()
     datos['vector_nombres'] = datos['NOMBRES'].apply(word_tokenize)
     datos['primer_nombre'] = datos['vector_nombres'].map(lambda x: x[0] if len(x) > 0 else None)
@@ -58,14 +52,18 @@ def nuevo_estan_cruzados(datos):
     datos['APELLIDOS'] = datos["APELLIDOS"].str.strip().apply(unidecode.unidecode).str.upper()
     datos['vector_apellidos'] = datos['APELLIDOS'].apply(word_tokenize)
     datos['primer_apellido'] = datos['vector_apellidos'].map(lambda x: x[0] if len(x) > 0 else None)
-    datos['pimer_apellido_es_nombre'] = datos['primer_apellido'].apply(lambda x: encontrar_similitudes(x, df['Nombre'].tolist()))
+    datos['primer_apellido_es_nombre'] = datos['primer_apellido'].apply(lambda x: encontrar_similitudes(x, df['Nombre'].tolist()))
 
     datos['segundo_apellido'] = datos['vector_apellidos'].apply(lambda x: x[1] if len(x) >= 2 else None)
     datos['segundo_apellido_es_nombre'] = datos['segundo_apellido'].apply(lambda x: encontrar_similitudes(x, df['Nombre'].tolist()))
 
+    # Evaluate cross-checking results
     datos['estan_cruzados'] = datos.apply(verificar_cruzados, axis=1)
 
-    eliminar = ['vector_nombres', 'primer_nombre', 'pimer_nombre_es_apellido', 'segundo_nombre', 'segundo_nombre_es_apellido', 'vector_apellidos', 'primer_apellido', 'pimer_apellido_es_nombre', 'segundo_apellido', 'segundo_apellido_es_nombre']
+    # Drop unnecessary columns
+    eliminar = ['vector_nombres', 'primer_nombre', 'pimer_nombre_es_apellido', 'segundo_nombre',
+                'segundo_nombre_es_apellido', 'vector_apellidos', 'primer_apellido', 
+                'primer_apellido_es_nombre', 'segundo_apellido', 'segundo_apellido_es_nombre']
     datos.drop(columns=eliminar, inplace=True)
     return datos
 
@@ -87,14 +85,12 @@ async def validar_nombres_apellidos():
         si_rows_count = ((df['Nombre_Invalido'] == 'SI') | (df['Apellido_Invalido'] == 'SI') | (df['estan_cruzados'] == 'SI')).sum()
         no_rows_count = ((df['Nombre_Invalido'] == 'NO') & (df['Apellido_Invalido'] == 'NO') & (df['estan_cruzados'] == 'NO')).sum()
 
-
         message = (
             f"Validación de nombres y apellidos: \n"
             f"{no_rows_count} Nombres Y Apellidos Correctos. \n"
             f"{si_rows_count} Nombres Y Apellidos Incorrectos. \n"
-            
         )
 
         return PlainTextResponse(content=message)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Un error occurrio: {e}")
+        raise HTTPException(status_code=500, detail=f"Un error ocurrió: {str(e)}")
