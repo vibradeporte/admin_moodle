23554f78233d2343b82ddbcee247016e11871099
vibradeporte <info@univlearning.com>
2024-08-20 23:08:33 -0500

    Update core_user_update_users.py

--- routers/core_user_update_users.py
+++ routers/core_user_update_users.py
@@ -3,37 +3,85 @@ from pydantic import BaseModel
 import requests
 import pandas as pd
 import os
+import pycountry
 
 core_user_update_users_router = APIRouter()
 
 WS_FUNCTION = "core_user_update_users"
+
+def obtener_codigo_iso_pais(nombre_pais: str) -> str | None:
+    try:
+        pais = pycountry.countries.get(name=nombre_pais)
+        return pais.alpha_2 if pais else 'SIN PAIS'
+    except KeyError:
+        return None
+
 
 @core_user_update_users_router.post("/core_user_update_users/", tags=['Moodle'], status_code=200)
 async def core_user_update_users(moodle_url: str = Form(...), moodle_token: str = Form(...)):
 
     data = {}
     df = pd.read_csv('temp_files/estudiantes_validados.csv')
-    
+    df["country"] = df["country"].apply(obtener_codigo_iso_pais)
+    if df is None:
+        raise HTTPException(status_code=400, detail="No se encontr  el archivo 'temp_files/estudiantes_validados.csv'")
+
     for i, row in df.iterrows():
         USERID = row.get("userid")
-        USERNAME = row.get("username", "")
-        FIRSTNAME = row.get("firstname", "")
-        LASTNAME = row.get("lastname", "")
-        EMAIL = row.get("email", "")
-        CITY = row.get("city", "")
-        COUNTRY = row.get("country", "")
-        DESCRIPTION = row.get("description", "")
+
+        if USERID is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'userid'")
+        
+        USERNAME = row.get("username")
+        if USERNAME is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'username'")
+        
+        FIRSTNAME = row.get("firstname")
+        if FIRSTNAME is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'firstname'")
+        
+        LASTNAME = row.get("lastname")
+        if LASTNAME is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'lastname'")
+        
+        EMAIL = row.get("email")
+        if EMAIL is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'email'")
+        
+        CITY = row.get("city")
+        if CITY is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'city'")
+        
+        COUNTRY = row.get("country")
+        if COUNTRY is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'country'")
+        
+        DESCRIPTION = row.get("description")
+        if DESCRIPTION is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'description'")
+        
         FIRSTNAMEPHONETIC = ""
-        LASTNAMEPHONETIC = row.get("lastnamephonetic","")
+        LASTNAMEPHONETIC = row.get("lastnamephonetic")
+        if LASTNAMEPHONETIC is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'lastnamephonetic'")
+        
         MIDDLENAME = ""
         ALTERNATENAME = ""
         INTERESTS = ""
-        IDNUMBER = row.get("username", "")
+        IDNUMBER = row.get("username")
+        if IDNUMBER is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'username'")
+        
         INSTITUTION = ""
         DEPARTMENT = ""
         PHONE1 = row.get("phone1")
+        if PHONE1 is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'phone1'")
+        
         PHONE2 = ""
-        ADDRESS = row.get("address", "")
+        ADDRESS = row.get("address")
+        if ADDRESS is None:
+            raise HTTPException(status_code=400, detail="El archivo 'temp_files/estudiantes_validados.csv' no tiene la columna 'address'")
 
         data[f"users[{i}][id]"] = int(USERID)
         data[f"users[{i}][username]"] = USERNAME
@@ -68,3 +116,4 @@ async def core_user_update_users(moodle_url: str = Form(...), moodle_token: str 
         raise HTTPException(status_code=response.status_code, detail=response.text)
 
     return {"output": response.json()}
+
