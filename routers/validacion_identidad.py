ba958d8a38bd4e79a1143c19d2f2ac6a610de9f3
vibradeporte <info@univlearning.com>
2024-08-26 09:21:52 -0500

    Update validacion_identidad.py

--- routers/validacion_identidad.py
+++ routers/validacion_identidad.py
@@ -28,28 +28,30 @@ def get_db():
 @identificacion_usuario.get("/user/{user_id}", tags=['Validacion_Usuario'])
 def encontrar_usuario(user_id: int, db = Depends(get_db)):
     query = text("""
-    SELECT
-    c.URL_MOODLE as URL_MOODLE,
-    c.TOKEN_MOODLE as TOKEN_MOODLE,
-    c.PREFIJO_TABLAS as PREFIJO_TABLAS,
-    c.MOTOR_BD as MOTOR_BD,
-    c.SERVIDOR as SERVIDOR_BD,
-    c.PUERTO as PUERTO_BD,
-    c.USUARIO as USUARIO_BD,
-    c.CONTRASENA as CONTRASENA_BD,
-    c.NOMBRE_BD as NOMBRE_BD,
-    u.ID_USUARIO as ID_USUARIO,
-    u.NOMBRE as NOMBRE,
-    u.APELLIDO as APELLIDO,
-    u.MOVIL as MOVIL,
-    u.CORREO as CORREO,
-    cu.`FID_CASO_USO-CLIENTE` as ID_PERMISO
-    FROM
-        USUARIO AS u
-    JOIN CLIENTE AS c ON u.FID_CLIENTE = c.ID_CLIENTE
-    JOIN `CASO_USO-USUARIO` AS cu ON u.ID_USUARIO = cu.FID_USUARIO
-    WHERE
-        u.IDENTIFICACION = :IDENTIFICACION;
+        SELECT
+            c.URL_MOODLE as URL_MOODLE,
+            c.TOKEN_MOODLE as TOKEN_MOODLE,
+            c.PREFIJO_TABLAS as PREFIJO_TABLAS,
+            c.MOTOR_BD as MOTOR_BD,
+            c.SERVIDOR as SERVIDOR_BD,
+            c.PUERTO as PUERTO_BD,
+            c.USUARIO as USUARIO_BD,
+            c.CONTRASENA as CONTRASENA_BD,
+            c.NOMBRE_BD as NOMBRE_BD,
+            u.ID_USUARIO as ID_USUARIO,
+            u.NOMBRE as NOMBRE,
+            u.APELLIDO as APELLIDO,
+            u.MOVIL as MOVIL,
+            u.CORREO as CORREO,
+            cu.`FID_CASO_USO-CLIENTE` as ID_PERMISO
+        FROM
+            USUARIO AS u
+        JOIN CLIENTE AS c ON u.FID_CLIENTE = c.ID_CLIENTE
+        JOIN `CASO_USO-USUARIO` AS cu ON u.ID_USUARIO = cu.FID_USUARIO
+        WHERE
+            u.IDENTIFICACION = :IDENTIFICACION
+            AND
+        	cu.`FID_CASO_USO-CLIENTE` = 1;
     """)
     
 
