50d6fc7147b5fa3bbddd19b921e630355b1f37fa
Maria Navas <mafenavas5@gmail.com>
2024-09-13 15:11:31 -0500

    cambios en la funcion de lectura y manipulacion de estudiantes matriculados para traer los datos necesarios, adicionalmente de poner las columnas de mensajes de Whatsapp y correo bienvenida

--- routers/poblar_tabla_detalle_matricula.py
+++ routers/poblar_tabla_detalle_matricula.py
@@ -56,29 +56,59 @@ def estatus_envio_correo():
         print("No se obtuvieron datos de analytics.")
         return []  # Retornar una lista vacía si no hay datos
     
+
 def estudiantes_matriculados():
     """
-    Devuelve un DataFrame con los estudiantes que se han matriculado con éxito
+    Devuelve un DataFrame con los estudiantes que se han matriculado con éxito, con todas las columnas convertidas a tipo str antes de aplicar fillna.
     """
     ruta_archivo = 'temp_files/estudiantes_validados.csv'
+    
+    # Verificar si el archivo existe
     if not os.path.exists(ruta_archivo):
-        df = pd.DataFrame()
-        return df
+        print(f"Archivo no encontrado: {ruta_archivo}")
+        return pd.DataFrame()
 
+    # Leer el archivo CSV
     df = pd.read_csv(ruta_archivo)
-
+    
+    # Verificar si el DataFrame está vacío
     if df.empty:
-        return df
+        print("El archivo CSV existe pero está vacío.")
+        return pd.DataFrame()
 
+    # Si falta la columna NRO_DIAS_DE_MATRICULAS, agregarla
     if 'NRO_DIAS_DE_MATRICULAS' not in df.columns:
         df['NRO_DIAS_DE_MATRICULAS'] = 0
 
+    # Mantener solo las columnas de interés
     columnas_interes = ['username', 'TIPO_IDENTIFICACION', 'firstname', 'lastname', 'email',
                         'MOVIL', 'country', 'city', 'EMPRESA', 'CORREO_SOLICITANTE', 
                         'NRO_DIAS_DE_MATRICULAS', 'NOMBRE_CORTO_CURSO']
+    
+    # Verificar si las columnas de interés están en el DataFrame
+    columnas_faltantes = [col for col in columnas_interes if col not in df.columns]
+    if columnas_faltantes:
+        print(f"Faltan columnas en el DataFrame: {columnas_faltantes}")
+        return pd.DataFrame()  # Retornar un DataFrame vacío si faltan columnas importantes
+    
+    # Filtrar las columnas de interés
     df = df[columnas_interes]
+    
+    # Verificar si después de filtrar hay datos
+    if df.empty:
+        print("El DataFrame está vacío después de filtrar las columnas de interés.")
+        return df
+    
+    # Eliminar filas donde todas las columnas de interés sean NaN
     df.dropna(subset=columnas_interes, how='all', inplace=True)
+    
+    # Convertir todas las columnas a tipo str antes de aplicar fillna
+    df = df.astype(str)
+
+    # Reemplazar valores NaN por 'SIN DATOS'
     df.fillna('SIN DATOS', inplace=True)
+    df.replace('nan', 'SIN DATOS', inplace=True)
+    # Renombrar columnas
     df = df.rename(columns={
         'username': 'IDENTIFICACION',
         'firstname': 'NOMBRES',
@@ -88,7 +118,8 @@ def estudiantes_matriculados():
         'country': 'PAIS_DEL_MOVIL',
         'city': 'CIUDAD'
     })
-    
+
+    # Verificación de los mensajes de correo
     if os.path.exists('temp_files/message_ids.csv'):
         df_mensajes_correo = estatus_envio_correo()
 
@@ -107,9 +138,39 @@ def estudiantes_matriculados():
         # En caso de que no exista el archivo o df_mensajes_correo no sea un DataFrame
         df['RES_CORREO_BIENVENIDA'] = 'NO ENVIADO AL CORREO'
 
-        df['RES_MATRICULA'] = 'MATRICULADO'
 
-        return df
+    # Verificación de los mensajes de WhatsApp
+    if os.path.exists('temp_files/message_status_wapp.csv'):
+        # Leer el archivo CSV con solo las columnas necesarias
+        df_mensajes_wapp = pd.read_csv('temp_files/message_status_wapp.csv', usecols=['message_status', 'numero'])
+
+        # Renombrar las columnas
+        df_mensajes_wapp = df_mensajes_wapp.rename(columns={'message_status': 'RES_WS_BIENVENIDA', 'numero': 'MOVIL'})
+
+        # Asegurarse de que la columna 'MOVIL' esté en formato string
+        df_mensajes_wapp['MOVIL'] = df_mensajes_wapp['MOVIL'].astype(str)
+
+        # Eliminar los dos primeros caracteres de la columna 'MOVIL'
+        df_mensajes_wapp['MOVIL'] = df_mensajes_wapp['MOVIL'].str.replace(r'^.{2}', '', regex=True)
+
+        # Verificar si la columna 'MOVIL' existe en ambos DataFrames
+        if 'MOVIL' in df.columns and 'MOVIL' in df_mensajes_wapp.columns:
+            # Realizar la unión de los DataFrames basándote en la columna 'MOVIL'
+            df = pd.merge(df, df_mensajes_wapp, on='MOVIL', how='left')
+        else:
+            print("Error: La columna 'MOVIL' no existe en ambos DataFrames.")
+    else:
+        df['RES_WS_BIENVENIDA'] = 'NO ENVIADO AL WHATSAPP'
+    
+    df['RES_MATRICULA'] = 'MATRICULADO'
+    
+    # Asegurarse de eliminar la columna 'id' si existe
+    if 'id' in df.columns:
+        df.drop(columns=['id'], inplace=True)
+
+    return df
+
+
 
 def estudiantes_no_matriculados():
     """
@@ -117,17 +178,37 @@ def estudiantes_no_matriculados():
     """
     invalid_students_file_path = 'temp_files/estudiantes_invalidos.xlsx'
 
+    # Verificar si el archivo existe
+    if not os.path.exists(invalid_students_file_path):
+        print(f"Archivo no encontrado: {invalid_students_file_path}")
+        return pd.DataFrame()
+
+    # Leer el archivo Excel
     df = pd.read_excel(invalid_students_file_path)
+
+    # Verificar si el DataFrame está vacío
     if df.empty:
-        return df
+        print("El archivo Excel existe pero está vacío.")
+        return pd.DataFrame()
+
+    # Mantener solo las columnas de interés
     columnas_interes = ['username', 'TIPO_IDENTIFICACION', 'firstname', 'lastname', 'email',
                        'MOVIL', 'country', 'city', 'EMPRESA', 'CORREO_SOLICITANTE', 
                        'NOMBRE_CORTO_CURSO']
     
+    # Eliminar filas donde todas las columnas de interés sean NaN
     df.dropna(subset=columnas_interes, how='all', inplace=True)
+    
+    # Convertir todas las columnas a tipo str
+    df = df.astype(str)
+
+    # Reemplazar valores NaN por 'SIN DATOS'
     df.fillna('SIN DATOS', inplace=True)
+    df.replace('nan', 'SIN DATOS', inplace=True)
+    # Filtrar las columnas de interés
     df = df[columnas_interes]
 
+    # Renombrar columnas
     df = df.rename(columns={
         'username': 'IDENTIFICACION',
         'firstname': 'NOMBRES',
@@ -138,7 +219,7 @@ def estudiantes_no_matriculados():
         'city': 'CIUDAD'
     })
 
-
+    # Agregar las columnas extra
     extra_columns = pd.DataFrame({
         'RES_MATRICULA': ['NO MATRICULADO'] * len(df),
         'RES_CORREO_BIENVENIDA': ['NO MATRICULADO'] * len(df),
@@ -153,28 +234,27 @@ def estudiantes_no_matriculados():
 @poblar_tabla_detalle_matricula_router.post("/poblar_tabla_detalle_matricula/{fid_matricula}", response_model=int, tags=['Base de Datos'])
 def create_matricula(fid_matricula: int):
     # Obtener los datos de estudiantes matriculados y no matriculados
-    estudiantes_matriculados_df = estudiantes_matriculados()  # Función que obtiene estudiantes matriculados
+    estudiantes_matriculados_df = estudiantes_matriculados() 
+    print(estudiantes_matriculados_df) # Función que obtiene estudiantes matriculados
     estudiantes_no_matriculados_df = estudiantes_no_matriculados()  # Función que obtiene estudiantes no matriculados
     
     # Asegurarse de que ambas funciones devuelvan DataFrames y no None
-    if estudiantes_matriculados_df is None:
-        estudiantes_matriculados_df = pd.DataFrame()
-    if estudiantes_no_matriculados_df is None:
-        estudiantes_no_matriculados_df = pd.DataFrame()
+    estudiantes_matriculados_df = estudiantes_matriculados_df if estudiantes_matriculados_df is not None else pd.DataFrame()
+    estudiantes_no_matriculados_df = estudiantes_no_matriculados_df if estudiantes_no_matriculados_df is not None else pd.DataFrame()
     
     # Verificar si ambos DataFrames están vacíos
     if estudiantes_matriculados_df.empty and estudiantes_no_matriculados_df.empty:
         raise HTTPException(status_code=400, detail="No hay datos para insertar")
-
-    # Concatenar solo si ambos DataFrames no están vacíos
+    
+    # Concatenar ambos DataFrames si ambos tienen datos, o utilizar solo el que tenga datos
     if not estudiantes_matriculados_df.empty and not estudiantes_no_matriculados_df.empty:
-        df = pd.concat([estudiantes_matriculados_df, estudiantes_no_matriculados_df], ignore_index=False)
-    elif not estudiantes_matriculados_df.empty:  # Solo hay datos de estudiantes matriculados
+        df = pd.concat([estudiantes_matriculados_df, estudiantes_no_matriculados_df], ignore_index=True)
+    elif not estudiantes_matriculados_df.empty:  # Solo estudiantes matriculados tiene datos
         df = estudiantes_matriculados_df
-    elif not estudiantes_no_matriculados_df.empty:  # Solo hay datos de estudiantes no matriculados
+    elif not estudiantes_no_matriculados_df.empty:  # Solo estudiantes no matriculados tiene datos
         df = estudiantes_no_matriculados_df
     
-    # Agregar la columna 'FID_MATRICULA'
+    # Agregar la columna 'FID_MATRICULA' a todos los registros
     df['FID_MATRICULA'] = fid_matricula
 
     try:
